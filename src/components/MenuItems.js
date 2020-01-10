2b9fc440fce784cf8e8c593c415e4200016462b6
SanusiMD <just4deen@yahoo.com>
2020-01-10 17:24:04 +0100

    updated Menu Page

---
+++ src/components/MenuItems.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+
+class MenuItems extends Component {
+  render() {
+    return (
+      <div className="menuItems">
+        <div className="container">
+        <div className="menuItemBox">
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+          </div>
+          <div className="menuItemBox">
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+          </div>
+          <div className="menuItemBox">
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+            <div className="menuItem">
+              <img
+                src={require("../img/d-foodie-basket.png")}
+                alt=""
+                className="menuItem__food"
+              />
+              <div className="menuItem__info">
+                <div className="menuItem__info--details">
+                  <h4> Chinese Egg Cake </h4>
+                  <h4> $8.5 </h4>
+                </div>
+                <p>Chine murtard/Chopitle</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuItems;
--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,17 +1,28 @@
-import React, {Component } from 'react'
-import TopBox from '../TopBox'
-import Footer from '../Footer'
+import React, { Component } from "react";
+import TopBox from "../TopBox";
+import HeadingStyle from "../HeadingStyle";
+import Footer from "../Footer";
+import MenuItems from "../MenuItems";
+import Recommend from "../Recommend";
 
-class Menu extends Component{
-    render(){
-        return(
-            <div>
-                <TopBox  title="Menu"/>
-                <h1> Menu Page </h1>
-                <Footer/>
-            </div>
-        )
-    }
+class Menu extends Component {
+  render() {
+    return (
+      <div>
+        <TopBox title="Menu" />
+        <HeadingStyle maintitle="Breakfast" subtitle="Food for Morning" />
+        <MenuItems />
+        <div className="menuLunch">
+        <HeadingStyle maintitle="Lunch" subtitle="Delicious Lunch items" />
+        <MenuItems />
+        </div>
+        <HeadingStyle maintitle="Dinner" subtitle="The Mael of the Day" />
+        <MenuItems />
+        <Recommend />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Menu;
